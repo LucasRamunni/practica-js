678d28938c33c565cb92669f82269e0e8025cb21
Pablo Marino <pmarino2013@gmail.com>
2020-10-14 20:36:30 -0300

    Actualizacion de archivos y limpieza de codigo

--- logueo/app.js
+++ logueo/app.js
@@ -5,71 +5,64 @@
 //  localStorage.setItem("logueo", JSON.stringify(usuario));
 //  localStorage.setItem('autenticado', 'false')
 
-
 let botoncito = document.getElementById("botones");
 
 //let logueo = false;
-let logueo = localStorage.getItem('autenticado')
-
-
+let logueo = localStorage.getItem("autenticado");
 
 const validacion = (logueo) => {
-    let pagina = location.pathname
-    if (pagina === "/admin.html") {
-        if (logueo === "false") {
-            $('#exampleModalCenter').modal('show')
-            location = "index.html"
-        }
+  let pagina = location.pathname;
+  if (pagina === "/admin.html") {
+    if (logueo === "false") {
+      // $('#exampleModalCenter').modal('show')
+      location = "index.html";
     }
-}
+  }
+};
 
 const loguear = () => {
-    if (logueo === 'true') {
-        botoncito.innerHTML = botonCerrar;
-    } else {
-        botoncito.innerHTML = botonInicio;
-
-    }
-
-
+  if (logueo === "true") {
+    botoncito.innerHTML = botonCerrar;
+  } else {
+    botoncito.innerHTML = botonInicio;
+  }
 };
 
 const logueoChange = () => {
-    let usuario = document.getElementById("user").value;
-    let password = document.getElementById("password").value;
+  let usuario = document.getElementById("user").value;
+  let password = document.getElementById("password").value;
 
-    let dato = JSON.parse(localStorage.getItem("logueo"));
-    if (usuario === "" || password === "") {
-
-        return alert('DEbe ingresar valores en los campos')
-    }
-    if (dato.nombre === usuario) {
-        if (dato.password === password) {
-            logueo = true;
-            loguear();
-            localStorage.setItem('autenticado', 'true')
-            window.location = 'admin.html'
-        } else {
-            alert("Usuario o contraseña incorrectos");
-        }
+  let dato = JSON.parse(localStorage.getItem("logueo"));
+  if (usuario === "" || password === "") {
+    return alert("Debe ingresar valores en los campos");
+  }
+  if (dato.nombre === usuario) {
+    if (dato.password === password) {
+      logueo = true;
+      loguear();
+      localStorage.setItem("autenticado", "true");
+      window.location = "admin.html";
     } else {
-        alert("Usuario o contraseña incorrectos");
+      alert("Usuario o contraseña incorrectos");
     }
+  } else {
+    alert("Usuario o contraseña incorrectos");
+  }
 
-    document.getElementById("user").value = "";
-    document.getElementById("password").value = "";
-    //   logueo = !logueo;
-    //   if (!logueo) {
-    //     alert("Se ha deslogueado con éxito");
-    //   }
-    //   loguear();
+  document.getElementById("user").value = "";
+  document.getElementById("password").value = "";
+  //   logueo = !logueo;
+  //   if (!logueo) {
+  //     alert("Se ha deslogueado con éxito");
+  //   }
+  //   loguear();
 };
 const desloguear = () => {
-    logueo = false;
-    alert("Has sido deslogueado");
-    localStorage.setItem('autenticado', 'false')
-    loguear();
-    window.location = "index.html"
+  logueo = false;
+  alert("Has sido deslogueado");
+  localStorage.setItem("autenticado", "false");
+  loguear();
+  window.location = "index.html";
 };
 let botonInicio = `<button
               id="botonLogueo"
@@ -89,4 +82,4 @@ let botonCerrar = `<button
               Cerrar sesión
             </button>`;
 loguear();
-validacion(logueo)
+validacion(logueo);
